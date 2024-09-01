b2ac6ed4ed945bc3dd8d2d9b1b7452e8d8ca436a
iamrakeshanand <rakeshanand2004@gmail.com>
2024-09-01 11:46:55 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import io
+from flask_cors import CORS
+
+application = Flask(__name__)
+CORS(application)
+
+# Load the trained model
+model = load_model('mood_detection_model.h5')
+
+# Define a dictionary to map class indices to mood labels
+class_labels = {0: 'happy', 1: 'sad', 2: 'neutral'}
+
+@application.route('/predict', methods=['POST'])
+def predict():
+    print("hello")
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    try:
+        # Load and preprocess the image
+        img = image.load_img(io.BytesIO(file.read()), target_size=(128, 128))
+        img_array = image.img_to_array(img) / 255.0
+        img_array = np.expand_dims(img_array, axis=0)
+
+        # Predict the mood
+        predictions = model.predict(img_array)
+        mood_index = np.argmax(predictions[0])
+        mood = class_labels.get(mood_index, 'unknown')
+
+        return jsonify({'mood': mood})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0',port=80, debug=True)
